f0993c7ef493c9628eea49f1309c259126067928
zonblade <agungzonblade@gmail.com>
2024-12-19 07:07:57 +0700

    working bootable

--- rotor/src/main.rs
+++ rotor/src/main.rs
@@ -1,21 +1,13 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-mod sys;
+use log::info;
+use uefi::prelude::*;
 
-static HELLO: &[u8] = b"Hello this is working bootable!";
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+    info!("Hello world!");
+    boot::stall(10_000_000);
+    Status::SUCCESS
 }
-
